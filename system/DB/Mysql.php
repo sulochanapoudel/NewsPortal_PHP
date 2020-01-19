957d4bb9f8185413e5ca70bb0b41fb61bf923a5d
sulochanapoudel <sulochanamuna@gmail.com>
2020-01-19 13:20:52 +0545

    DAY 3

--- system/DB/Mysql.php
+++ system/DB/Mysql.php
@@ -6,5 +6,16 @@ namespace System\DB;
 
 class Mysql
 {
+    private $conn;
+    private $result;
 
+
+    public function __construct()
+    {
+       $this->conn = new \PDO("mysql:host:localhost;dbname=demo", "root", "");
+    }
+    public function run($sql)
+    {
+
+    }
 }
